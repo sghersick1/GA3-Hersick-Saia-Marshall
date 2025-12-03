c59e8156ed45ad87ccb319d15076a74b16abca39
sghersick1 <hersicks@gmail.com>
2025-12-03 17:56:57 -0500

    Added in my_sql & Junit dependencies. Added in DAO test file.

---
+++ src/test/java/edu/loyola/cs485/model/dao/ClientDaoTest.java
@@ -0,0 +1,16 @@
+package edu.loyola.cs485.model.dao;
+
+import org.junit.jupiter.api.*;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ClientDaoTest {
+
+    @Test
+    public void testFake(){
+        assertTrue(true);
+    }
+}
