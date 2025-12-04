80444d051a3eea37c835d125a85644291d6b9094
chrissaia <144486134+chrissaia@users.noreply.github.com>
2025-12-04 11:58:42 -0500

    Refactor PlaylistDAO to PlaylistDao in tests

--- src/test/java/edu/loyola/cs485/model/dao/PlaylistDAOTest.java
+++ src/test/java/edu/loyola/cs485/model/dao/PlaylistDAOTest.java
@@ -1,7 +1,7 @@
 package edu.loyola.cs485.model.dao;
 
 import edu.loyola.cs485.model.entity.Playlist;
-import edu.loyola.cs485.model.dao.PlaylistDAO;
+import edu.loyola.cs485.model.dao.PlaylistDao;
 import org.junit.jupiter.api.*;
 
 import java.util.List;
@@ -11,7 +11,7 @@ import java.sql.Timestamp;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-public class PlaylistDAOTest {
+public class PlaylistDaoTest {
 
     @Test
     public void testFake(){
@@ -22,7 +22,7 @@ public class PlaylistDAOTest {
 
     @Test
     public void testCreatePlaylist() throws Exception{
-        PlaylistDAO dao = new PlaylistDAO();
+        PlaylistDao dao = new PlaylistDao();
         dao.setTestDatabase();
 
         Playlist playlist = new Playlist();
@@ -32,40 +32,39 @@ public class PlaylistDAOTest {
         dao.create(playlist); // Method Under Test
 
         //clean up
-        dao.delete( playlist.getID() );
+        dao.delete( playlist.getId() );
 
         assertAll(
-                () -> assertNotNull( playlist.getID() )
+                () -> assertNotNull( playlist.getId() )
         );
     }
 
     @Test
     public void testReadPlaylist() throws Exception{
-        PlaylistDAO dao = new PlaylistDAO();
+        PlaylistDao dao = new PlaylistDao();
         dao.setTestDatabase();
 
         Playlist playlist = new Playlist();
         playlist.setName("Test Playlist");
-        playlist.setEmail("test@email.com");
+        playlist.setCreationTimestamp( new Timestamp(System.currentTimeMillis()) );
 
         dao.create(playlist);
-        Playlist found = dao.read(playlist.getID()); //Function under test
+        Playlist found = dao.read(playlist.getId()); //Function under test
 
         //clean up
         dao.delete( playlist.getID() );
 
         assertAll(
-                () -> assertEquals(found.getID(), playlist.getID()),
+                () -> assertEquals(found.getId(), playlist.getId()),
                 () -> assertEquals(found.getName(), playlist.getName()),
-                () -> assertEquals(found.getEmail(), playlist.getEmail()),
-                () -> assertEquals(found.getDob(), playlist.getDob())
+                () -> assertEquals(found.getCreationTimestamp(), playlist.getCreationTimestamp())
         );
 
     }
 
     @Test
     public void testReadPlaylistDoesNotExist() throws Exception{
-        PlaylistDAO dao = new PlaylistDAO();
+        PlaylistDao dao = new PlaylistDao();
         dao.setTestDatabase();
 
         Playlist found = dao.read(10);
@@ -77,7 +76,7 @@ public class PlaylistDAOTest {
 
     @Test
     public void testListPlaylist() throws Exception{
-        PlaylistDAO dao = new PlaylistDAO();
+        PlaylistDao dao = new PlaylistDao();
         dao.setTestDatabase();
 
         List<Playlist> lst = dao.list();
