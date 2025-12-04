61a0c84e3728fb0d3779381f3f2e31375e81f46e
chrissaia <144486134+chrissaia@users.noreply.github.com>
2025-12-04 11:15:15 -0500

    h

--- src/test/java/edu/loyola/cs485/model/AbstractDAOTest.java
+++
@@ -1,6 +0,0 @@
-public abstract class AbstractDAO<E extends AbstractEntity> {
-
-//Adding this method to the AbstractDAO class
-   public void setTestDB(){
-     this.Database ="music_db_test"; //Adding _test to DB Name
-}
